416c5c01231a509d112c2fe927c8b174b69764fe
AishaSaleem13 <aishasaleem1308@gmail.com>
2025-08-13 04:51:05 +0500

    update

--- index.mjs
+++ index.mjs
@@ -1,15 +1,17 @@
 import express from "express";
-import db from './config/db.mjs'
-import router from './routes/mainindex.mjs'
-db.connection.once('open', () => console.log("connected to db")).on("error", (err) => console.log("error connecting db -->", err))
-import cros from 'cors'
+import db from './config/db.mjs';
+import router from './routes/mainindex.mjs';
+import cors from 'cors';
 
-const app = express()
+db.connection
+  .once('open', () => console.log("connected to db"))
+  .on("error", (err) => console.log("error connecting db -->", err));
 
-app.listen(3000,()=>{
-    console.log(`server is running port `);
-})
-app.use(express.json())
+const app = express();
 
-app.use(cros())
-app.use('/',router)
+app.use(express.json());
+app.use(cors());
+app.use('/', router);
+
+// Vercel ke liye export
+export default app;
