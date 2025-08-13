047938301834c21b0f885b52f7b98babd70aeeac
AishaSaleem13 <aishasaleem1308@gmail.com>
2025-08-13 05:02:37 +0500

    done

--- index.mjs
+++ index.mjs
@@ -1,25 +1,16 @@
 import express from "express";
-import db from './config/db.mjs';
-import router from './routes/mainindex.mjs';
-import cors from 'cors';
+import db from './config/db.mjs'
+import router from './routes/mainindex.mjs'
+db.connection.once('open', () => console.log("connected to db")).on("error", (err) => console.log("error connecting db -->", err))
+import cros from 'cors'
 
-db.connection
-  .once('open', () => console.log("connected to db"))
-  .on("error", (err) => console.log("error connecting db -->", err));
+const app = express()
 
-const app = express();
+// app.listen(3000,()=>{
+//     console.log(`server is running port `);
+// })
+app.use(express.json())
 
-app.use(express.json());
-app.use(cors());
-app.use('/', router);
-
-// Local testing ke liye
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-
-// Vercel ke liye export
-export default app;
+app.use(cros())
+app.use('/',router)
+export default app
