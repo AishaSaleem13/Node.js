24b54affe5f2935a8d146bb00f99bd056d5f3ab3
AishaSaleem13 <aishasaleem1308@gmail.com>
2025-09-19 05:23:57 +0500

    changes

--- index.mjs
+++ index.mjs
@@ -4,6 +4,8 @@ import router from './routes/mainindex.mjs'
 db.connection.once('open', () => console.log("connected to db")).on("error", (err) => console.log("error connecting db -->", err))
 import cors from 'cors'
 
+
+
 const app = express()
 
 // app.listen(5000,()=>{
@@ -17,6 +19,12 @@ app.use(express.json())
 app.get("/", (req, res) => {
   res.send("blah blah bl;ah ✅");
 });
+
+if(process.env.NODE_ENV !=="production"){
+ app.listen(5000,()=>{
+    console.log(`server is running port `);
+})
+}
 
 app.use('/',router)
 export default app
